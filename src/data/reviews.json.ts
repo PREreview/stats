ff558fca638673309cad969d0fac7b260ef41b81
Chris Wilkinson <c.wilkinson@elifesciences.org>
2024-07-02 16:32:47 +0100

    Make review preprint server data available
    Refs #28

--- src/data/reviews.json.ts
+++ src/data/reviews.json.ts
@@ -4,6 +4,7 @@ import { Schema } from '@effect/schema'
 import { Config, Effect, Redacted } from 'effect'
 import * as Doi from '../lib/Doi.js'
 import * as LanguageCode from '../lib/LanguageCode.js'
+import * as PreprintServer from '../lib/PreprintServer.js'
 import * as Temporal from '../lib/Temporal.js'
 
 const Reviews = Schema.Array(
@@ -11,6 +12,7 @@ const Reviews = Schema.Array(
     createdAt: Temporal.PlainDateFromStringSchema,
     preprint: Doi.ParseDoiSchema,
     language: Schema.optional(LanguageCode.LanguageCodeSchema),
+    server: PreprintServer.PreprintServerSchema,
   }),
 )
 
---
+++ src/lib/PreprintServer.ts
@@ -0,0 +1,35 @@
+import { Schema } from '@effect/schema'
+import type { Predicate } from 'effect'
+
+export type PreprintServer = keyof typeof preprintServers
+
+const preprintServers = {
+  africarxiv: 'AfricArXiv Preprints',
+  arxiv: 'arXiv',
+  authorea: 'Authorea',
+  biorxiv: 'bioRxiv',
+  chemrxiv: 'ChemRxiv',
+  eartharxiv: 'EarthArXiv',
+  ecoevorxiv: 'EcoEvoRxiv',
+  edarxiv: 'EdArXiv',
+  engrxiv: 'engrXiv',
+  medrxiv: 'medRxiv',
+  metaarxiv: 'MetaArXiv',
+  osf: 'OSF',
+  'osf-preprints': 'OSF Preprints',
+  philsci: 'PhilSci-Archive',
+  'preprints.org': 'Preprints.org',
+  psyarxiv: 'PsyArXiv',
+  psycharchives: 'PsychArchives',
+  'research-square': 'Research Square',
+  scielo: 'SciELO Preprints',
+  'science-open': 'ScienceOpen Preprints',
+  socarxiv: 'SocArXiv',
+  techrxiv: 'TechRxiv',
+  zenodo: 'Zenodo',
+} as const
+
+const isPreprintServer: Predicate.Refinement<unknown, PreprintServer> = (u): u is PreprintServer =>
+  typeof u === 'string' && Object.hasOwn(preprintServers, u)
+
+export const PreprintServerSchema = Schema.String.pipe(Schema.filter(isPreprintServer))
