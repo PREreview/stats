a3a766f1f0797486195a07590429e0cbf87e1401
Chris Wilkinson <c.wilkinson@elifesciences.org>
2024-07-30 11:57:18 +0100

    Make the names of clubs available
    Refs #76

---
+++ src/data/clubs.json.ts
@@ -0,0 +1,36 @@
+import { HttpClient, HttpClientRequest, HttpClientResponse, Terminal } from '@effect/platform'
+import { NodeTerminal } from '@effect/platform-node'
+import { Schema } from '@effect/schema'
+import { Config, Effect, Record, Redacted } from 'effect'
+
+const Clubs = Schema.Array(
+  Schema.Struct({
+    id: Schema.String,
+    name: Schema.String,
+  }),
+)
+
+const Output = Schema.Record(Schema.String, Schema.String)
+
+const program = Effect.gen(function* () {
+  const terminal = yield* Terminal.Terminal
+  const token = yield* Config.redacted('PREREVIEW_REVIEWS_DATA_TOKEN')
+
+  const request = HttpClientRequest.bearerToken(
+    HttpClientRequest.get('https://prereview.org/clubs-data'),
+    Redacted.value(token),
+  )
+
+  const data = yield* HttpClient.fetchOk(request).pipe(
+    Effect.andThen(HttpClientResponse.schemaBodyJson(Clubs)),
+    Effect.scoped,
+  )
+
+  const transformedData = Record.fromIterableWith(data, club => [club.id, club.name])
+
+  const encoded = yield* Schema.encode(Schema.parseJson(Output))(transformedData)
+
+  yield* terminal.display(encoded)
+})
+
+await Effect.runPromise(program.pipe(Effect.provide(NodeTerminal.layer)))
