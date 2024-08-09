65844e0dd81663211874a17e4f4f2d089e9c9023
Chris Wilkinson <chris@prereview.org>
2024-08-09 09:22:42 +0100

    Ensure there are in-browser errors
    Refs ad53282373feda48d3c34d57783e26e51cbc75a7

--- integration/base.ts
+++ integration/base.ts
@@ -1,10 +1,27 @@
-import { type Fixtures, type PlaywrightTestArgs, type PlaywrightTestOptions, test as baseTest } from '@playwright/test'
+import {
+  type Fixtures,
+  type PlaywrightTestArgs,
+  type PlaywrightTestOptions,
+  test as baseTest,
+  expect,
+} from '@playwright/test'
 
 export { expect } from '@playwright/test'
 
 const appFixtures: Fixtures<Record<never, never>, Record<never, never>, PlaywrightTestArgs & PlaywrightTestOptions> = {
   baseURL: async ({}, use) => {
     await use('http://localhost:3000')
+  },
+  context: async ({ context }, use) => {
+    const errors: Array<string> = []
+
+    context.on('weberror', error => {
+      errors.push(error.error().message)
+    })
+
+    await use(context)
+
+    expect(errors).toHaveLength(0)
   },
 }
 
