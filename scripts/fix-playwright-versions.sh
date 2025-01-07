45c8d70f58d8db2a917912a3d589aa31575a1cd5
Chris Wilkinson <chris@prereview.org>
2025-01-07 11:46:19 +0000

    Match the Playwright version in CI

---
+++ scripts/fix-playwright-versions.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -e
+
+version=$(npm list --depth=0 --json --package-lock-only | jq --raw-output '.dependencies."@playwright/test".version')
+
+echo "Found @playwright/test $version"
+
+echo "Updating CI"
+sed --in-place --regexp-extended "s/mcr\.microsoft\.com\/playwright:v.+?-jammy/mcr.microsoft.com\/playwright:v$version-jammy/g" .github/workflows/ci.yml
