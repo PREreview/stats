5187d58ddfab534343983d6088fde94920062a42
Chris Wilkinson <chris@prereview.org>
2025-09-04 09:50:49 +0100

    Paginate through the list
    Refs https://github.com/PREreview/coar-notify/issues/961

--- src/data/requests.json.ts
+++ src/data/requests.json.ts
@@ -1,13 +1,13 @@
-import { HttpClient, HttpClientRequest, HttpClientResponse, Terminal } from '@effect/platform'
+import { HttpClient, HttpClientResponse, Terminal } from '@effect/platform'
 import { NodeHttpClient, NodeTerminal } from '@effect/platform-node'
-import { Effect, Schema } from 'effect'
+import { Effect, Option, pipe, Schema, Stream } from 'effect'
 import * as Doi from '../lib/Doi.js'
 import * as LanguageCode from '../lib/LanguageCode.js'
 import { DomainIdSchema, FieldIdSchema, SubfieldIdSchema } from '../lib/OpenAlex.js'
 import * as PreprintServer from '../lib/PreprintServer.js'
 import * as Temporal from '../lib/Temporal.js'
 
-const Requests = Schema.Array(
+const Requests = Schema.Chunk(
   Schema.Struct({
     timestamp: Temporal.InstantFromStringSchema,
     preprint: Doi.DoiSchema,
@@ -20,18 +20,17 @@ const Requests = Schema.Array(
 )
 
 const program = Effect.gen(function* () {
-  const client = yield* HttpClient.HttpClient
   const terminal = yield* Terminal.Terminal
 
-  const request = HttpClientRequest.get('https://coar-notify.prereview.org/requests')
-
-  const data = yield* client
-    .execute(request)
-    .pipe(
+  const data = yield* Stream.paginateChunkEffect(1, page =>
+    pipe(
+      HttpClient.get('https://coar-notify.prereview.org/requests', { urlParams: { page } }),
       Effect.andThen(HttpClientResponse.filterStatusOk),
       Effect.andThen(HttpClientResponse.schemaBodyJson(Requests)),
       Effect.scoped,
-    )
+      Effect.andThen(results => [results, Option.andThen(Option.fromIterable(results), page + 1)]),
+    ),
+  ).pipe(Stream.runCollect)
 
   const encoded = yield* Schema.encode(Schema.parseJson(Requests))(data)
 
